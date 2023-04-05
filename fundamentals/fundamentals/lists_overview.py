51983ad1ec0a6b000ed8e51592a05b15050ac4d8
winterilee <plumtree9612@gmail.com>
2023-04-04 21:09:07 -0700

    studying List Overview

---
+++ fundamentals/fundamentals/lists_overview.py
@@ -0,0 +1,16 @@
+ninjas = ['Rozen', 'KB', 'Oliver']
+my_list = ['4', ['list', 'in', 'a', 'list'], 987]
+empty_list = []
+
+fruits = ['apple', 'banana', 'orange', 'strawberry']
+vegetables = ['lettuce', 'cucumber', 'carrots']
+fruits_and_vegetables = fruits + vegetables
+print(fruits_and_vegetables)
+salad = 3 * vegetables
+print(salad)
+
+salad2 = salad + [fruits[2]]
+print(salad2)
+
+salad2.append(fruits[1])
+print(salad2)
