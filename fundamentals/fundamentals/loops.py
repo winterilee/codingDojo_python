9b9b419a1be5e1feab98cae6ae837402009fc714
winterilee <plumtree9612@gmail.com>
2023-04-05 02:00:01 -0700

    studying Loops

---
+++ fundamentals/fundamentals/loops.py
@@ -0,0 +1,19 @@
+for i in range(5):
+    print(i)
+
+for i in range(2, 7):
+    print(i)
+
+for i in range(2, 16, 3):
+    print(i)
+
+for x in range(0, 10, 2):
+    print(x)
+
+for x in range(5, 1, -3):
+    print(x)
+
+# Challenge: Write a for loop to print all integers from 1 to 20, including 20.
+
+for i in range(1, 21):
+    print(i)
