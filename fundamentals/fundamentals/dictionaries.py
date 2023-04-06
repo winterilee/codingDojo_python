53560b692b1a42793b72209d84c4396734c883fe
winterilee <plumtree9612@gmail.com>
2023-04-05 23:14:11 -0700

    studying Dictionaries

---
+++ fundamentals/fundamentals/dictionaries.py
@@ -0,0 +1,33 @@
+#literal notation
+person = {"first": "Ada", "last": "Lovelace", "age": 42, "is_organ_donor": True}
+capitals = {} #create an empty dictionary then add values
+capitals["svk"] = "Bratislava"
+capitals["deu"] = "Berlin"
+capitals["dnk"] = "Copenhagen"
+
+# List Syntax:
+# my_list[0] = 4
+
+# Dictionary Syntax:
+# my_dict["some_string"] = some_value
+
+
+# Challenge yourself!
+
+person = {"first_name": "Ada", "last_name": "Lovelace", "age": 42, "is_organ_donor": True}
+# Create another person dictionary called person_2 and print it to the terminal
+person_2 = {"first_name": "Winter", "last_name": "Lee", "favorite_color": "Cerulean", "favorite_food": "pasta"}
+
+print(person_2)
+
+capitals = {}
+capitals["svk"] = "Bratislava"
+capitals["deu"] = "Berlin"
+capitals["dnk"] = "Copenhagen"
+# Add 2 key-value pairs to this dictionary.
+capitals["sko"] = "Seoul"
+capitals["can"] = "Ottawa"
+
+# print the capitals dictionary to see how it changed!
+
+print(capitals)
