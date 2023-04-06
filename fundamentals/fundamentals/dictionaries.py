039139e49fcb0ea0327f41217eb8f1032bd4393c
winterilee <plumtree9612@gmail.com>
2023-04-06 02:01:55 -0700

    continue studying Dictionaries

--- fundamentals/fundamentals/dictionaries.py
+++ fundamentals/fundamentals/dictionaries.py
@@ -31,3 +31,41 @@ capitals["can"] = "Ottawa"
 # print the capitals dictionary to see how it changed!
 
 print(capitals)
+
+
+# Adds a new key value pair for email to person
+person["email"] = "alovelace@codingdojo.com"
+
+# Changes person's "last_name" value to "Bobada"
+person["last_name"] = "Bobada"
+print(person)
+
+if "email" not in person:
+    person["email"] = "newemail@email.com"
+else:
+    print("Would you like to replace your existing email?")
+
+
+countries_so_far = {"Mexico": 1, "Morocco": 1}
+new_visits = ["Albania", "Mexico", "Togo", "Morocco"]
+    
+# To add Albania to the list
+countries_so_far["Albania"] = 1
+# To add 1 to the Mexico tally
+countries_so_far["Mexico"] += 1 # Changes Mexico tally to 2!
+# your code here to finish updating your travel log!
+countries_so_far["Togo"] = 1
+countries_so_far["Morocco"] += 1
+print(countries_so_far)
+
+
+# Accessing Values
+print(person["first_name"])
+full_name = person["first_name"] + " " + person["last_name"]
+print(full_name)
+
+
+# Removing Values
+value_removed = capitals.pop('svk') # will remove the key 'svk' and return it's value
+del capitals['dnk'] # will delete the key, and not return anything
+print(capitals)
