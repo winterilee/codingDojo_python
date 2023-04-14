f324ac0d79a906f2db0a162c3084ac2ddde9a81c
winterilee <plumtree9612@gmail.com>
2023-04-13 18:14:22 -0700

    created an entry for the assignment: Modular Practice, and added parent.py

---
+++ fundamentals/oop/modularizing/parent.py
@@ -0,0 +1,14 @@
+local_val = "magical unicorns"
+def square(x):
+    return x * x
+
+class User:
+    def __init__(self, name):
+        self.name = name
+    def say_hello(self):
+        return "hello"
+
+print(square(5))
+user = User("Anna")
+print(user.name)
+print(user.say_hello())
