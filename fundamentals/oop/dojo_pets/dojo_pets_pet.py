b8852954c0b5a4eded67b3bb2da98766c412f989
winterilee <plumtree9612@gmail.com>
2023-04-13 21:38:16 -0700

    completed the assignment: Dojo Pets

--- fundamentals/oop/dojo_pets/dojo_pets_pet.py
+++ fundamentals/oop/dojo_pets/dojo_pets_pet.py
@@ -39,3 +39,38 @@ class Pet:
 
 
 # TODO NINJA BONUS: Use Modules to separate out the classes into different files.
+
+# TODO SENSEI BONUS: Use Inheritance to create sub classes of pets
+
+class Pet_hotel(Pet):
+    def __init__(self, name, type, tricks, sound):
+        super().__init__(name, type, tricks, sound)
+    
+    def display_stat(self):
+        print(f"Current health of {self.name}: {self.health}")
+        print(f"Current energy of {self.name}: {self.energy}")
+        print("-" * 30)
+        return self
+
+    def nap_time(self):
+        self.energy += 12
+        super().sleep()
+        return self
+
+    def meal_time(self):
+        self.energy += 6
+        self.health += 8
+        super().eat()
+        return self
+
+    def play_time(self):
+        self.health += 3
+        super().play()
+        return self
+
+pet1 = Pet_hotel("Avocado","Dog", "Play dead", "woooof")
+pet2 = Pet_hotel("Crouton", "Cat", "Sneak attack", "meowwwwww")
+
+
+pet1.play_time().meal_time().nap_time().display_stat()
+pet2.meal_time().nap_time().display_stat()
