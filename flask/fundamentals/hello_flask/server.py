7c16db1de1b11b0498e1d07dbbad443f2e20f6ce
winterilee <plumtree9612@gmail.com>
2023-04-25 19:30:06 -0700

    continue studying Flask

--- flask/fundamentals/hello_flask/server.py
+++ flask/fundamentals/hello_flask/server.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template("index.html", phrase="hello", times=5)
 
+@app.route('/lists')
+def render_lists():
+    student_info = [
+        {'name' : 'Michael', 'age' : 35},
+        {'name' : 'John', 'age' : 30 },
+        {'name' : 'Mark', 'age' : 25},
+        {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], students = student_info)
+
+
+
 if __name__ == "__main__":
     app.run(debug = True, port = 5001)
