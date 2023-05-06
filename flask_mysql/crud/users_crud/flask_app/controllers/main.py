ebe1c16672d5b259efa1c6137f89966b670793d8
winterilee <plumtree9612@gmail.com>
2023-05-05 20:53:40 -0700

    completed the assignment: Users CRUD

---
+++ flask_mysql/crud/users_crud/flask_app/controllers/main.py
@@ -0,0 +1,41 @@
+from flask import render_template, request, redirect, session, url_for
+from flask_app import app
+from users import Users
+
+@app.route("/")
+def index():
+    return redirect("/users")
+
+@app.route("/users")
+def show_all():
+    users_list = Users.get_all()
+    return render_template("index.html", users = users_list)
+
+@app.route("/show_user/<int:user_id>")
+def show(user_id):
+    user_info = Users.get_one(user_id)
+    return render_template("display_user.html", user = user_info)
+
+@app.route("/new_user")
+def new_user():
+    return render_template("new_user.html")
+
+@app.route("/new_user/create", methods = ['POST'])
+def create():
+    Users.save(request.form)
+    return redirect("/users")
+
+@app.route("/user_update/<int:user_id>")
+def user_update(user_id):
+    user_info = Users.get_one(user_id)
+    return render_template("update_user.html", user = user_info)
+
+@app.route("/user_update/update", methods = ['POST'])
+def update():
+    Users.update(request.form)
+    return redirect("/users")
+
+@app.route("/user_delete/<int:user_id>")
+def delete(user_id):
+    Users.delete(user_id)
+    return redirect("/users")
---
+++ flask_mysql/crud/users_crud/users.py
@@ -0,0 +1,46 @@
+from mysqlconnection import connectToMySQL
+
+class Users:
+    DB = "users_schema"
+    def __init__(self, data):
+        self.id = data["id"]
+        self.first_name = data["first_name"]
+        self.last_name = data["last_name"]
+        self.email = data["email"]
+        self.created_at = data["created_at"]
+        self.updated_at = data["updated_at"]
+
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users;"
+        results = connectToMySQL(cls.DB).query_db(query)
+        user_list = []
+        for i in results:
+            user_list.append(cls(i))
+        return user_list
+    
+    @classmethod
+    def get_one(cls, user_id):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        data = {"id":user_id}
+        result = connectToMySQL(cls.DB).query_db(query, data)
+        return cls(result[0])
+    
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO users (first_name, last_name, email) VALUES (%(fname)s, %(lname)s, %(em)s);"
+        result = connectToMySQL(cls.DB).query_db(query, data)
+        return result
+
+    @classmethod
+    def update(cls, data):
+        query = "UPDATE users SET first_name = %(fname)s, last_name = %(lname)s, email = %(em)s WHERE id = %(id)s;"
+        result = connectToMySQL(cls.DB).query_db(query, data)
+        return result
+
+    @classmethod
+    def delete(cls, user_id):
+        query = "DELETE FROM users WHERE id = %(id)s;"
+        data = {"id":user_id}
+        result = connectToMySQL(cls.DB).query_db(query, data)
+        return result
