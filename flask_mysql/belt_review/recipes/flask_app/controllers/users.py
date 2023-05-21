0ec196933925ea04103b0bd44d22e70c330c03bc
winterilee <plumtree9612@gmail.com>
2023-05-20 19:20:17 -0700

    making a progress on the user side for the assignment: Recipes

--- flask_mysql/belt_review/recipes/flask_app/controllers/users.py
+++ flask_mysql/belt_review/recipes/flask_app/controllers/users.py
@@ -7,3 +7,41 @@ bcrypt = Bcrypt(app)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/create_user", methods = ["POST"])
+def create():
+    if not user.User.validate_user(request.form):
+        return redirect(url_for("index"))
+    else:
+        data = {
+            "first_name": request.form["first_name"],
+            "last_name": request.form["last_name"],
+            "email": request.form["email"],
+            "password": bcrypt.generate_password_hash(request.form["password"])
+        }
+        user_id = user.User.create_user(data)
+        user_in_db = user.User.get_one(user_id)
+        session["logged_in_id"] = user_in_db.id
+        session["logged_in_name"] = user_in_db.first_name
+        return redirect (url_for("dashboard"))
+
+@app.route("/login", methods = ["POST"])
+def login():
+    data = {"email": request.form["email"]}
+    user_in_db = user.User.get_by_email(data)
+    if not user_in_db:
+        flash("Invalid login.", "login")
+        return redirect(url_for("index"))
+    if not bcrypt.check_password_hash(user_in_db.password, request.form["password"]):
+        flash("Invalid login.", "login")
+        return redirect(url_for("index"))
+    session["logged_in_id"] = user_in_db.id
+    session["logged_in_name"] = user_in_db.first_name
+    return redirect (url_for("dashboard"))
+
+@app.route("/dashboard")
+def dashboard():
+    if "logged_in_id" not in session:
+        return redirect(url_for("index"))
+    #! place for get_all recipes
+    return render_template("dashboard.html")
